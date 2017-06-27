5b967c6ef0f96eafa1891802972c77b709d45ec8
Gabriel D. Karpman <gdkar@alum.mit.edu>
2017-06-27 07:08:45 -0400

    allow using containers other than std::initializer_list

--- subprocess.hpp
+++ subprocess.hpp
@@ -1056,10 +1056,11 @@ public:
 			execute_process();
 	}
 
-	template <typename... Args>
-	Popen(std::initializer_list<const char*> cmd_args, Args&& ...args)
+	template <template<class...> typename C, typename... Args>
+	Popen(C<const char*> cmd_args, Args&& ...args)
+    : vargs_(std::begin(cmd_args),std::end(cmd_args))
 	{
-		vargs_.insert(vargs_.end(), cmd_args.begin(), cmd_args.end());
+//		vargs_.insert(vargs_.end(), cmd_args.begin(), cmd_args.end());
 		init_args(std::forward<Args>(args)...);
 
 		// Setup the communication channels of the Popen class
@@ -1067,7 +1068,18 @@ public:
 		if (!defer_process_start_)
 			execute_process();
 	}
+	template <typename... Args>
+	Popen(std::initializer_list<const char*> cmd_args, Args&& ...args)
+    : vargs_(std::begin(cmd_args),std::end(cmd_args))
+	{
+//		vargs_.insert(vargs_.end(), cmd_args.begin(), cmd_args.end());
+		init_args(std::forward<Args>(args)...);
 
+		// Setup the communication channels of the Popen class
+		stream_.setup_comm_channels();
+		if (!defer_process_start_)
+			execute_process();
+	}
 	void start_process();
 
 	int pid() const noexcept
@@ -1716,12 +1728,16 @@ namespace detail
  * The parameters passed to the argument are exactly the same
  * one would use for Popen constructors.
  */
-template<typename... Args>
+template <template<class> typename C, typename... Args>
+int call(C<const char*> plist, Args&&... args)
+{
+	return (detail::call_impl(plist, std::forward<Args>(args)...));
+}
+template <typename... Args>
 int call(std::initializer_list<const char*> plist, Args&&... args)
 {
 	return (detail::call_impl(plist, std::forward<Args>(args)...));
 }
-
 template<typename... Args>
 int call(const std::string& arg, Args&&... args)
 {
@@ -1734,12 +1750,16 @@ int call(const std::string& arg, Args&&... args)
  * If the exit code was non-zero it raises a CalledProcessError.
  * The arguments are the same as for the Popen constructor.
  */
+template <template<class> typename C, typename... Args>
+OutBuffer check_output(C<const char*> plist, Args&&... args)
+{
+	return (detail::check_output_impl(plist, std::forward<Args>(args)...));
+}
 template <typename... Args>
 OutBuffer check_output(std::initializer_list<const char*> plist, Args&&... args)
 {
 	return (detail::check_output_impl(plist, std::forward<Args>(args)...));
 }
-
 template <typename... Args>
 OutBuffer check_output(const std::string& arg, Args&&... args)
 {
